aa99c9ba68ad1867715fef1f302d891fcd9c77e2
junsukesakamoto <75307865+junsukesakamoto@users.noreply.github.com>
2020-12-12 19:20:37 +0900

    Update App.jsx
    コメントを追加

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,18 @@ export default class App extends React.Component{
   constructor(props){
       super(props);
       this.state={
-        answers:[],
-        chats:[],
-        currentId:"init",
-        dataset:defaultDataset,
-        open:false
+        answers:[],   //回答コンポーネントに表示するデータ
+        chats:[],     //チャットコンポーネントに表示するデータ
+        currentId:"init", //現在の質問ID
+        dataset:defaultDataset, //質問と回答のデータセット
+        open:false  //問い合わせフォーム用モーダルの開閉を管理
       }
       this.selectAnswer = this.selectAnswer.bind(this)
       this.handleClose = this.handleClose.bind(this)
       this.handleClickOpen = this.handleClickOpen.bind(this)
   }
 
+  //次の質問をチャットエリアに表示する関数
   displayNextQuestion = (nextQuestionId) =>{
     const chats = this.state.chats
     chats.push({
@@ -33,7 +34,7 @@ export default class App extends React.Component{
       currentId:nextQuestionId
     })
   }
-
+  //回答が選択された時に呼ばれる関数
   selectAnswer = (selectedAnswer,nextQuestionId) =>{
     switch(true){
       case(nextQuestionId==='init'):
@@ -43,15 +44,17 @@ export default class App extends React.Component{
       case(nextQuestionId === 'contact'):
         this.handleClickOpen();
         break;
-
+      //リンクが選択された時そのリンクに飛ばしてあげる関数
       case(/^http*/.test(nextQuestionId)):
         const a = document.createElement('a');
         a.href = nextQuestionId;
         a.target = '_blank';
         a.click();
         break;
+      //選択された回答をchatsに追加
       default:
         const chats= this.state.chats;
+        //現在のチャット一覧を取得
         chats.push({
           text: selectedAnswer,
           type: 'answer'
@@ -64,20 +67,20 @@ export default class App extends React.Component{
         break;
     }
   }
-
+//クリックで問い合わせモーダルを開く
 handleClickOpen = () => {
     this.setState({open:true});
 };
-
+//問い合わせモーダルを閉じる
 handleClose = () => {
     this.setState({open:false});
 };
-
+  //回答を空白で初期化
   componentDidMount(){
     const initAnswer = "";  
     this.selectAnswer(initAnswer,this.state.currentId)
   }
-
+  //回答が増えたら自動スクロール
   componentDidUpdate(prevProps,prevState,snapshot){
       const scrollArea=document.getElementById('scroll-area')
       if(scrollArea){
@@ -89,7 +92,7 @@ handleClose = () => {
     return(
       <div>
         <section className="c-section">
-          <div className="c-box">
+          <div className="c-box">　
             <Chats chats={this.state.chats}/>
             <AnswersList answers={this.state.answers} select={this.selectAnswer}/>
             <FormDialog open={this.state.open} handleClose={this.handleClose}/>
@@ -99,4 +102,3 @@ handleClose = () => {
     );
   }
 }
-
